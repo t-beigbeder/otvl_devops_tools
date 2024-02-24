92c24a5a32e804280a5903c3d7d5ca69a0535866
thierry_otvl <tbeigbeder.nospam@gmail.com>
2024-02-24 14:58:08 +0100

    mci

--- src/shell/otvl/otvl_cloud_init_from_git.sh
+++ src/shell/otvl/otvl_cloud_init_from_git.sh
@@ -5,7 +5,21 @@ enableswap() {
   fallocate -l 1G /swapfile && \
   chmod 600 /swapfile && \
   mkswap /swapfile && \
-  echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+  echo "/swapfile swap swap defaults 0 0" >> /etc/fstab && \
+  true
+}
+
+enablevenv() {
+  if [ -d /srv/venv/otvl_cloud_init ] ; then return 0 ; fi
+  virtualenv -p python3 /srv/venv/otvl_cloud_init && \
+  /srv/venv/otvl_cloud_init/bin/pip install pyudev PyYAML cryptography && \
+  true
+}
+
+getosmeta() {
+  if [ -f /root/clinit/osmeta ] ; then return 0 ; fi
+  curl http://169.254.169.254/openstack/2018-08-27/meta_data.json | jq .meta > /root/clinit/osmeta && \
+  true
 }
 
 git_repo="https://github.com/t-beigbeder/otvl_devops_tools"
@@ -13,7 +27,7 @@ git_branch="bdev1"
 git_local="/root/clinit/otvl_devops_tools"
 echo `date`: command $0 is starting
 
-mkdir /root/bin
+mkdir -p /root/bin /srv/venv /srv/otvl/iaas/python
 cat > /root/bin/otvl_display_net_conf.sh <<EOF
 #!/bin/sh
 logger -t  otvl_display_net_conf -s "command $0 is starting"
@@ -52,16 +66,35 @@ for device in context.list_devices():
     sys.stderr.write("otvl_cloud_init_py_check: listing {0}\n".format(device))
 EOF
 
-systemctl enable /etc/systemd/system/otvl_display_net_conf.service && \
-virtualenv -p python3 /srv/venv/otvl_cloud_init && \
-/srv/venv/otvl_cloud_init/bin/pip install pyudev && \
-/srv/venv/otvl_cloud_init/bin/python /root/otvl_cloud_init_py_check.py && \
-tmp=`ip -4 -o address show | grep dynamic` && \
-external_ip=`echo $tmp | cut -d' ' -f4 | cut -d/ -f1` && \
-nic_dev=`echo $tmp | cut -d' ' -f2` && \
-enableswap && \
-true || exit 1
+cat > /etc/systemd/system/otvl_network_configurator.service <<EOF
+[Unit]
+Description=Runs otvl_network_configurator
+Wants=otvl_network_configurator.timer
 
+[Service]
+ExecStart=/srv/venv/otvl_cloud_init/bin/python /srv/otvl/iaas/python/otvl_network_configurator.py
+WorkingDirectory=/srv/otvl/iaas/python
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat > /etc/systemd/system/otvl_network_configurator.timer <<EOF
+[Unit]
+Description=Run otvl_network_configurator every 2 minutes
+Requires=otvl_network_configurator.service
+
+[Timer]
+Unit=otvl_network_configurator.service
+OnCalendar=*:0/2
+
+[Install]
+WantedBy=timers.target
+EOF
+
+tmp=`ip -4 -o address show | grep dynamic`
+external_ip=`echo $tmp | cut -d' ' -f4 | cut -d/ -f1`
+nic_dev=`echo $tmp | cut -d' ' -f2`
 cat > /etc/network/interfaces <<EOF
 # This file describes the network interfaces available on your system
 # and how to activate them. For more information, see interfaces(5).
@@ -76,6 +109,18 @@ iface lo inet loopback
 allow-hotplug $nic_dev
 iface $nic_dev inet dhcp
 EOF
+
+sed -i -e 's=#precedence ::ffff:0:0/96  100=precedence ::ffff:0:0/96  100=' /etc/gai.conf
+
+systemctl enable /etc/systemd/system/otvl_display_net_conf.service && \
+virtualenv -p python3 /srv/venv/otvl_cloud_init && \
+/srv/venv/otvl_cloud_init/bin/pip install pyudev && \
+/srv/venv/otvl_cloud_init/bin/python /root/otvl_cloud_init_py_check.py && \
+enableswap && \
+enablevenv && \
+cp src/python/otvl/otvl_network_configurator.py /srv/otvl/iaas/python/ && \
+getosmeta && \
+true || exit 1
 
 echo `date`: command $0 is exiting || exit 1
 exit 0
