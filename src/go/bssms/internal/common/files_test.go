aff4ce17cc2a141a3b56ea80165cedec999a2bef
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-01-05 17:27:51 +0100

    fixed non root test

--- src/go/bssms/internal/common/files_test.go
+++ src/go/bssms/internal/common/files_test.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"github.com/stretchr/testify/require"
+	"os"
 	"path/filepath"
 	"testing"
 )
@@ -16,9 +17,13 @@ func TestCheckDir(t *testing.T) {
 	require.Equal(t, AsInterfaceSlice(true, nil), AsInterfaceSlice(CheckDir(filepath.Join(tp, "level1/createall"), true, true, 0o777)))
 	l1l2 := filepath.Join(tp, "level1/level2")
 	require.Equal(t, AsInterfaceSlice(true, nil), AsInterfaceSlice(CheckDir(l1l2, true, true, 0o500)))
-	require.NoError(t, ExecuteSimple("chattr", "+i", l1l2))
+	if os.Getuid() == 0 {
+		require.NoError(t, ExecuteSimple("chattr", "+i", l1l2))
+	}
 	r, err = CheckDir(filepath.Join(tp, "level1/level2/refused"), true, false, 0o777)
 	require.Equal(t, false, r)
 	require.Error(t, err)
-	require.NoError(t, ExecuteSimple("chattr", "-i", l1l2))
+	if os.Getuid() == 0 {
+		require.NoError(t, ExecuteSimple("chattr", "-i", l1l2))
+	}
 }
