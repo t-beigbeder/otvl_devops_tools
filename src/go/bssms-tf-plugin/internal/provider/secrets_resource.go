dd3561d681156f35e4424953aa3e3b1a44ffce11
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-01-14 21:59:49 +0100

    tfx

--- src/go/bssms-tf-plugin/internal/provider/secrets_resource.go
+++ src/go/bssms-tf-plugin/internal/provider/secrets_resource.go
@@ -4,10 +4,12 @@ import (
 	"bssms/bssms"
 	"bssms/provisioner"
 	"context"
+	"errors"
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"net"
 )
 
 // Ensure the implementation satisfies the expected interfaces.
@@ -40,12 +42,12 @@ func (r *secretsResource) Schema(_ context.Context, _ resource.SchemaRequest, re
 				ElementType: types.StringType,
 				Required:    true,
 			},
-			"mac_ext_address": schema.StringAttribute{Optional: true},
+			"mac_ext_address": schema.StringAttribute{Required: true},
 			"ip_int_addresses": schema.ListAttribute{
 				ElementType: types.StringType,
 				Required:    true,
 			},
-			"mac_int_address": schema.StringAttribute{Optional: true},
+			"mac_int_address": schema.StringAttribute{Required: true},
 			"secrets": schema.MapAttribute{
 				ElementType: types.StringType,
 				Required:    true,
@@ -72,20 +74,37 @@ func (r *secretsResource) Configure(_ context.Context, req resource.ConfigureReq
 	r.pc = pc.pc
 }
 
-func makeInstallHost(model secretsResourceModel) (provisioner.InstallHost, error) {
-	ih := provisioner.InstallHost{
+func getIPV4(addresses []types.String) (string, error) {
+	for _, address := range addresses {
+		ip := net.ParseIP(address.ValueString())
+		if ip != nil && ip.To4() != nil {
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("no IPv4 address found")
+}
+
+func makeInstallHost(model secretsResourceModel) (ih provisioner.InstallHost, err error) {
+	ih = provisioner.InstallHost{
 		Installable: bssms.Installable{
 			Name:          model.Name.ValueString(),
 			ServerUuid:    model.ServerUuid.ValueString(),
 			MacExtAddress: model.MacExtAddress.ValueString(),
 			MacIntAddress: model.MacIntAddress.ValueString(),
-			IPExtAddress:  "",
-			IPIntAddress:  "",
 		},
 		PubKey:  model.PubKey.ValueString(),
 		Secrets: map[string]string{},
 	}
-	return ih, nil
+	if ih.IPExtAddress, err = getIPV4(model.IPExtAddresses); err != nil {
+		return
+	}
+	if ih.IPIntAddress, err = getIPV4(model.IPIntAddresses); err != nil {
+		return
+	}
+	for k, v := range model.Secrets.Elements() {
+		ih.Secrets[k] = v.String()
+	}
+	return
 }
 
 func (r *secretsResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
@@ -115,21 +134,76 @@ func (r *secretsResource) Create(ctx context.Context, req resource.CreateRequest
 		)
 		return
 	}
+
+	// Set state to fully populated data
+	diags = resp.State.Set(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 }
 
-func (r *secretsResource) Read(ctx context.Context, request resource.ReadRequest, response *resource.ReadResponse) {
-	//TODO implement me
-	panic("implement me")
+func (r *secretsResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	// Get current state
+	var state secretsResourceModel
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	//Set refreshed state
+	diags = resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 }
 
-func (r *secretsResource) Update(ctx context.Context, request resource.UpdateRequest, response *resource.UpdateResponse) {
-	//TODO implement me
-	panic("implement me")
+func (r *secretsResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	// Retrieve values from plan
+	var plan secretsResourceModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	ih, err := makeInstallHost(plan)
+	if err != nil {
+		resp.Diagnostics.AddError("Error updating InstallHost", err.Error())
+		return
+	}
+	err = provisioner.RunIhs(
+		&bssms.ProvisionerConfig{
+			BaseConfig:   bssms.BaseConfig{ctx},
+			UnsafeTls:    r.pc.UnsafeTls,
+			ProxyAddress: r.pc.ProxyAddress,
+		},
+		[]provisioner.InstallHost{ih})
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error creating Secrets",
+			fmt.Sprintf("RunIhs: %s", err),
+		)
+		return
+	}
+
+	// Set state to fully populated data
+	diags = resp.State.Set(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 }
 
-func (r *secretsResource) Delete(ctx context.Context, request resource.DeleteRequest, response *resource.DeleteResponse) {
-	//TODO implement me
-	panic("implement me")
+func (r *secretsResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	// Retrieve values from state
+	var state secretsResourceModel
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 }
 
 // secretsResourceModel maps the resource schema data.
@@ -139,7 +213,7 @@ type secretsResourceModel struct {
 	ServerUuid     types.String   `tfsdk:"server_uuid"`
 	IPExtAddresses []types.String `tfsdk:"ip_ext_addresses"`
 	MacExtAddress  types.String   `tfsdk:"mac_ext_address"`
-	IPIntAddress   []types.String `tfsdk:"ip_int_address"`
+	IPIntAddresses []types.String `tfsdk:"ip_int_addresses"`
 	MacIntAddress  types.String   `tfsdk:"mac_int_address"`
 	Secrets        types.Map      `tfsdk:"secrets"`
 }
