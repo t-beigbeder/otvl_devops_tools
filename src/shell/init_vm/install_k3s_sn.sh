1d905b6448126f69ab1345873f7f9deac384c30e
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-01-20 20:59:46 +0100

    wip

--- src/shell/init_vm/install_k3s_sn.sh
+++ src/shell/init_vm/install_k3s_sn.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
 
+## pre
+rp=`realpath $0`
+sd=`dirname $rp`
+. $sd/env_install.sh
+## endpre
+
+if [ -f /etc/systemd/system/k3s.service ] ; then
+  exit 0
+fi
+mkdir -p /etc/rancher/k3s && \
+  chmod 755 /etc/rancher/k3s && \
+  true
+# curl -sfL https://get.k3s.io | sh -s - --docker
--- src/shell/init_vm/install_rops_repo.sh
+++ src/shell/init_vm/install_rops_repo.sh
@@ -6,25 +6,8 @@ sd=`dirname $rp`
 . $sd/env_install.sh
 ## endpre
 
-sopsurl="https://github.com/getsops/sops/releases/download/v${IV_SOPS_VERSION}/sops-v${IV_SOPS_VERSION}.linux.amd64"
-
-installsops() {
-  is_root && \
-  cmd curl -L ${sopsurl} -o /usr/local/bin/sops && \
-  cmd chmod 755 /usr/local/bin/sops && \
-  true
-  return $?
-}
-
-if [ ! -f /usr/local/bin/sops ] ; then
-  installsops
-else
-  if [ "`sops -version|cut -d' ' -f2`" != "${IV_SOPS_VERSION}" ] ; then
-    log sops version mismatch "installed: `sops  -version|cut -d' ' -f2`" "wanted: ${IV_SOPS_VERSION}"
-    if [ "$IV_SOPS_UPDATE" ]; then
-      log "Installing sops from ${sopsurl}"
-      cmd rm /usr/local/bin/sops && \
-      installsops
-    fi
-  fi
+if [ ! -d /root/localgit/otvl_rops ] ; then
+  cd /root/localgit && \
+    GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=accept-new -i /root/.ssh/id_rsa_gh_ssh" git clone git@github.com:t-beigbeder/otvl_rops.git && \
+    true
 fi
