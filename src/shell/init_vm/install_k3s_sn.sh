3f2738f7097f2659d22ca78000d10f58baa0a06d
thierry_otvl <tbeigbeder.nospam@gmail.com>
2025-01-21 14:22:22 +0100

    wip

--- src/shell/init_vm/install_k3s_sn.sh
+++ src/shell/init_vm/install_k3s_sn.sh
@@ -11,6 +11,7 @@ install_k3s() {
   chmod 755 /etc/rancher/k3s /var/lib/rancher/k3s/server/manifests && \
   install_template /etc/rancher/k3s/rancher-config.yaml 644 "${CI_LHN}-loc" && \
   install_template /var/lib/rancher/k3s/server/manifests/traefik-config.yaml 644 && \
+  install_template /etc/rancher/k3s/registries.yaml 644 && \
   log "will run curl -sfL https://get.k3s.io | sh -s - --docker" && \
   curl -sfL https://get.k3s.io | sh -s - --docker && \
   true
